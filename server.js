981c82ab37e5a8be101bdd3493a096c7feecac71
Glitch (hello-express) <none>
2020-05-10 07:13:27 +0000

    👹⛳️ Checkpoint ./public/custom.css:15927824/31

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
