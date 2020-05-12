ffc0cca97e288f8e606ba7dcb45e4beb8d219cbe
Glitch (hello-express) <none>
2020-05-12 08:47:44 +0000

    👍🍝 Checkpoint ./controllers/user.controller.js:15927824/2044

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,46 +1,58 @@
-const shortid = require('shortid');
-var db = require('../db');
+const shortid = require("shortid");
+var db = require("../db");
 
 module.exports.index = function(req, res) {
-  res.render('users/index', {
-    users: db.get('users').value()
+  res.render("users/index", {
+    users: db.get("users").value()
   });
 };
 
 module.exports.add = function(req, res) {
-  res.render('users/add');
+  res.render("users/add");
 };
 
 module.exports.delete = function(req, res) {
   var id = req.params.id;
-  var item = db.get('users').find({ id: id }).value();
-  var index = db.get('users').indexOf(item).value();
-  
-  db.get('users').splice(index, 1).write();
-  
-  res.redirect('back');
+  var item = db
+    .get("users")
+    .find({ id: id })
+    .value();
+  var index = db
+    .get("users")
+    .indexOf(item)
+    .value();
+
+  db.get("users")
+    .splice(index, 1)
+    .write();
+
+  res.redirect("back");
 };
 
 module.exports.update = function(req, res) {
   var id = req.params.id;
-  res.render('users/update', {
+  res.render("users/update", {
     id: id
   });
 };
 
 module.exports.postAdd = function(req, res) {
   req.body.id = shortid.generate();
-  
-  db.get('users').push(req.body).write();
-  
-  res.redirect('/users');
+
+  db.get("users")
+    .push(req.body)
+    .write();
+
+  res.redirect("/users");
 };
 
 module.exports.postUpdate = function(req, res) {
   var id = req.params.id;
   var title = req.body.newTitle;
-  db.get('users').find({ id: id }).value().title = title;
-  
-  res.redirect('/users');
-};
+  db
+    .get("users")
+    .find({ id: id })
+    .value().title = title;
 
+  res.redirect("/users");
+};
