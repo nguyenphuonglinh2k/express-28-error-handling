c91c28467b4aa898b5e41e61ef8f2a6124898ca9
Glitch (hello-express) <none>
2020-05-13 08:35:59 +0000

    👙🎇 Checkpoint ./server.js:15927824/671 ./.gitignore:15927824/21

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ const app = express();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
-  to: 'nguyenphuonglinh11102000@gmail.com',
+  to: 'test@example.com',
   from: 'test@example.com',
   subject: 'Sending with Twilio SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
+
 sgMail.send(msg);
 
 app.set("views", "./views");
