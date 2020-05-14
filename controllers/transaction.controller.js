6ca956246a9e6c5ba8685f2b86c5060412c73c05
Glitch (hello-express) <none>
2020-05-14 09:07:45 +0000

    👨‍❤️‍💋‍👨🙋 Checkpoint

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -2,9 +2,12 @@ var db = require('../db');
 
 module.exports.index = function(req, res) {
   var userId = req.signedCookies.userId;
-
+  var page = parseInt(req.query.page) || 1;
+  var perPage = 8;
+  var drop = (page -1) * perPage;
+  
   res.render('transaction/index', {
-    transactions: db.get('transactions').value(),
+    transactions: db.get('transactions').drop(drop).take(perPage).value(),
     userId: userId
   });
 };
