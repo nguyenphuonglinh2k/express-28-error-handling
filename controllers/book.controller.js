1dd5ef241a341f5b3ccff007da3ebca8f34a1794
Glitch (hello-express) <none>
2020-05-20 08:56:40 +0000

    📠👵 Checkpoint ./controllers/book.controller.js:15927824/3453

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,9 +1,9 @@
-require('dotenv').config(); 
+require("dotenv").config();
 
-const shortid = require('shortid');
-var db = require('../db');
-var cloudinary = require('cloudinary').v2;
-var Book = require('../models/book.model.js');
+const shortid = require("shortid");
+var db = require("../db");
+var cloudinary = require("cloudinary").v2;
+var Book = require("../models/book.model.js");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -15,29 +15,39 @@ module.exports.index = function(req, res) {
   // res.render('book', {
   //   books: db.get('books').value()
   // });
-  
-  Book.find().then(books => console.log(books));
-  
-  // res.render('book');
+
+  Book.find().then(books =>
+    res.render("book", {
+      books: books
+    })
+  );
 };
 
 module.exports.add = function(req, res) {
-  res.render('add');
+  res.render("add");
 };
 
 module.exports.delete = function(req, res) {
   var id = req.params.id;
-  var item = db.get('books').find({ id: id }).value();
-  var index = db.get('books').indexOf(item).value();
-  
-  db.get('books').splice(index, 1).write();
-  
-  res.redirect('back');
+  var item = db
+    .get("books")
+    .find({ id: id })
+    .value();
+  var index = db
+    .get("books")
+    .indexOf(item)
+    .value();
+
+  db.get("books")
+    .splice(index, 1)
+    .write();
+
+  res.redirect("back");
 };
 
 module.exports.update = function(req, res) {
   var id = req.params.id;
-  res.render('update', {
+  res.render("update", {
     id: id
   });
 };
@@ -46,27 +56,30 @@ module.exports.postAdd = function(req, res) {
   var id = shortid.generate();
   var title = req.body.title;
   var des = req.body.description;
-  var image = 'https://22-sessionn.glitch.me/' + req.file.path.slice(7);
-  
-  cloudinary.uploader.upload(image, 
-    function(error, result) {
-      console.log(result, error);
-      db.get('books').push({ 
-        id: id, 
+  var image = "https://22-sessionn.glitch.me/" + req.file.path.slice(7);
+
+  cloudinary.uploader.upload(image, function(error, result) {
+    console.log(result, error);
+    db.get("books")
+      .push({
+        id: id,
         title: title,
         description: des,
         coverUrl: result.url
-      }).write();
+      })
+      .write();
   });
-  
-  res.redirect('/books');
+
+  res.redirect("/books");
 };
 
 module.exports.postUpdate = function(req, res) {
   var id = req.params.id;
   var title = req.body.newTitle;
-  db.get('books').find({ id: id }).value().title = title;
-  
-  res.redirect('/books');
-};
+  db
+    .get("books")
+    .find({ id: id })
+    .value().title = title;
 
+  res.redirect("/books");
+};
